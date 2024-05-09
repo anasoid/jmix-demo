35b69d7804ccf2f54adc4e3b2a5bd069db22e88c
anas <oufdou.anas@gmail.com>
2024-05-09 07:10:36 +0100

    init

---
+++ core/core/src/test/java/com/company/core/CoreTestConfiguration.java
@@ -0,0 +1,30 @@
+package com.company.core;
+
+import io.jmix.core.annotation.JmixModule;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import(CoreConfiguration.class)
+@PropertySource("classpath:/com/company/core/test-app.properties")
+@JmixModule(id = "com.company.core.test", dependsOn = CoreConfiguration.class)
+public class CoreTestConfiguration {
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.HSQL)
+                .build();
+    }
+}
